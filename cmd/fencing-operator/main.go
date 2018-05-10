be1338cfffc9b91fa6ee2d693b2dfa3dcf53d669
Andrew Beekhof <andrew@beekhof.net>
2018-05-11 09:33:13 +1000

    Option handling, including the ability to run in different modes

--- cmd/fencing-operator/main.go
+++ cmd/fencing-operator/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+	"flag"
 	"context"
 	"runtime"
 
@@ -11,6 +13,28 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+var (
+	namespace  string
+	name       string
+	mode       string
+
+	chaosLevel int
+
+	printVersion bool
+
+	createCRD bool
+)
+
+func init() {
+	//flag.StringVar(&debug.DebugFilePath, "debug-logfile-path", "", "only for a self hosted cluster, the path where the debug logfile will be written, recommended to be under: /var/tmp/etcd-operator/debug/ to avoid any issue with lack of write permissions")
+	flag.StringVar(&mode, "mode", "executioner", "Possible values: node watcher, executioner, [all]")
+	// chaos level will be removed once we have a formal tool to inject failures.
+	flag.IntVar(&chaosLevel, "chaos-level", -1, "DO NOT USE IN PRODUCTION - level of chaos injected into the etcd clusters created by the operator.")
+	flag.BoolVar(&printVersion, "version", false, "Show version and quit")
+	flag.BoolVar(&createCRD, "create-crd", true, "The operator will not create the EtcdCluster CRD when this flag is set to false.")
+	flag.Parse()
+}
+
 func printVersion() {
 	logrus.Infof("Go Version: %s", runtime.Version())
 	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
@@ -18,10 +42,33 @@ func printVersion() {
 }
 
 func main() {
+	namespace = os.Getenv(constants.EnvOperatorPodNamespace)
+	if len(namespace) == 0 {
+		logrus.Fatalf("must set env (%s)", constants.EnvOperatorPodNamespace)
+	}
+	name = os.Getenv(constants.EnvOperatorPodName)
+	if len(name) == 0 {
+		logrus.Fatalf("must set env (%s)", constants.EnvOperatorPodName)
+	}
+
 	printVersion()
-	sdk.Watch("fencing.clusterlabs.org/v1alpha1", "FencingSet", "default", 5)
-	sdk.Watch("k8s.io.api.core/v1", "Node", "default", 5)
-	sdk.Watch("k8s.io.api.core/v1", "Event", "default", 5)
+ 	if printVersion {
+		os.Exit(0)		
+	}
+
+	switch mode {
+	case "watcher":
+		sdk.Watch("fencing.clusterlabs.org/v1alpha1", "FencingSet", "default", 5)
+		sdk.Watch("k8s.io.api.core/v1", "Node", "default", 5)
+		sdk.Watch("k8s.io.api.core/v1", "Event", "default", 5)
+	case "executioner":
+		sdk.Watch("fencing.clusterlabs.org/v1alpha1", "FencingRequest", "default", 5)
+	case "all":
+		sdk.Watch("fencing.clusterlabs.org/v1alpha1", "FencingRequest", "default", 5)
+		sdk.Watch("fencing.clusterlabs.org/v1alpha1", "FencingSet", "default", 5)
+		sdk.Watch("k8s.io.api.core/v1", "Node", "default", 5)
+		sdk.Watch("k8s.io.api.core/v1", "Event", "default", 5)
+	}
 	sdk.Handle(stub.NewHandler())
 	sdk.Run(context.TODO())
 }
