a3dee9018f4821c692d9c4737372d47585678812
Andrew Beekhof <andrew@beekhof.net>
2018-05-25 11:19:50 +1000

    New constants

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -2,6 +2,7 @@ package constants
 
 const (
 	EnvOperatorPodName      = "POD_NAME"
-	EnvOperatorPodImage     = "POD_IMAGE"
 	EnvOperatorPodNamespace = "POD_NAMESPACE"
+	EnvOperatorPodMode      = "POD_MODE"
+	EnvOperatorPodImage     = "AGENT_IMAGE"
 )
